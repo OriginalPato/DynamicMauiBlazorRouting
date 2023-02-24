9490294bf3467c7391c81b332a7e2af59e9465df
jamesellis <James.Ellis@bybox.com>
2023-02-24 11:03:02 +0000

    Added simple react components to Blazor. Although we need to see what the overhead of this would be.

---
+++ DynamicBlazorUi/wwwroot/js/Counter.js
@@ -0,0 +1,13 @@
+﻿window.RenderProgressBar = (count) => {
+    const Progress = () => React.createElement(
+        Fabric.ProgressIndicator,
+        {
+            'label': 'React Counter',
+            'description': count,
+            'percentComplete': (count % 10) * 0.1
+        },
+        null
+    );
+
+    ReactDOM.render(Progress(), document.getElementById('reactProgressBar'));
+}
